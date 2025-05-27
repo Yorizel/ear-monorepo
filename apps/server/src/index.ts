f1638a5998cb2f9f0a7854364f02344d08e516e6
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-05-27 15:58:47 -0300

    test: Adds swagger endpoint

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,12 @@
 import cors from "@elysiajs/cors";
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
 const app = new Elysia()
   .use(cors())
+  .use(swagger())
   .get("/", () => "Hello Elysia")
+  .get("/hello", () => "Hello Elysia")
   .listen(9876);
 
 console.log(
