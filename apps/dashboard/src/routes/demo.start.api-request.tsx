cad7fe769c8dbd6b0bd5f42e93fb40a6038be1ce
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-05-28 15:33:41 -0300

    refactor: Simplify API request call

--- apps/dashboard/src/routes/demo.start.api-request.tsx
+++ apps/dashboard/src/routes/demo.start.api-request.tsx
@@ -1,21 +1,15 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-import { edenAdapter } from "@packages/eden";
 import { createFileRoute } from "@tanstack/react-router";
+import { edenAdapter } from "@packages/eden";
 async function getNames() {
-  const data = await edenAdapter.api.auth["sign-up"].post({
-    email: "yorizel@gmail.com",
-    name: "yorizel",
-    password: "12345678",
-  });
-  console.log(data);
+  const data = await edenAdapter.api.auth["sign-up"].post();
   return data;
 }
 
 export const Route = createFileRoute("/demo/start/api-request")({
   component: Home,
 });
-
 function Home() {
   useEffect(() => {
     getNames().then();
