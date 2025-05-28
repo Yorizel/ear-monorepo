f3fc7576b2452a72c11ffd25f7b966dc4d58813c
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-05-28 15:37:29 -0300

    refactor(auth): Update auth configuration

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,10 +1,12 @@
-import { db } from "@/database";
-import * as authSchema from "@/database/schemas/auth-schema";
+import * as authSchema from "@/schemas/auth-schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { openAPI } from "better-auth/plugins";
-import Elysia, { type Context } from "elysia";
+import Elysia from "elysia";
+import { db } from "./database";
+import { env } from "@/config/env";
 export const auth = betterAuth({
+  secret: env.BETTER_AUTH_SECRET,
   emailAndPassword: {
     enabled: true,
   },
