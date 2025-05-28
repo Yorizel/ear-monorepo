2b24ff0c3208ae2166fa7edf42c90f3c82bd1850
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-05-28 19:54:16 -0300

    refactor: Adopt biome and update formatting

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,19 +3,19 @@ import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { authService } from "./lib/auth";
 const app = new Elysia()
-  .use(authService)
-  .use(
-    cors({
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      credentials: true,
-      allowedHeaders: ["Content-Type", "Authorization"],
-    }),
-  )
-  .use(swagger())
-  .listen(9876);
+	.use(authService)
+	.use(
+		cors({
+			origin: "http://localhost:3000",
+			methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+			credentials: true,
+			allowedHeaders: ["Content-Type", "Authorization"],
+		}),
+	)
+	.use(swagger())
+	.listen(9876);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
 export type App = typeof app;
--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,21 +1,21 @@
+import { env } from "@/config/env";
 import * as authSchema from "@/schemas/auth-schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { openAPI } from "better-auth/plugins";
 import Elysia from "elysia";
 import { db } from "./database";
-import { env } from "@/config/env";
 export const auth = betterAuth({
-  secret: env.BETTER_AUTH_SECRET,
-  trustedOrigins: ["http://localhost:3000"],
-  emailAndPassword: {
-    enabled: true,
-  },
-  database: drizzleAdapter(db, {
-    provider: "pg",
-    schema: authSchema,
-  }),
-  plugins: [openAPI()],
+	secret: env.BETTER_AUTH_SECRET,
+	trustedOrigins: ["http://localhost:3000"],
+	emailAndPassword: {
+		enabled: true,
+	},
+	database: drizzleAdapter(db, {
+		provider: "pg",
+		schema: authSchema,
+	}),
+	plugins: [openAPI()],
 });
 
 export const authService = new Elysia().mount(auth.handler);
