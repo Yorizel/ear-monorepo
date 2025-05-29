a16e22795c997c0222a32652aafd28ef7591a51a
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-05-29 10:56:09 -0300

    build: Configure Astro image service

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    image: {
+        service: passthroughImageService(),
+    },
     integrations: [
         starlight({
             customCss: ["./src/styles/global.css"],
