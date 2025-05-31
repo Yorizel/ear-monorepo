8301cb1067e48a1905ad6621e418da388dfaf1d4
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-05-30 23:42:26 -0300

    feat: add loader to prefetch work data

--- apps/dashboard/src/routes/index.tsx
+++ apps/dashboard/src/routes/index.tsx
@@ -1,39 +1,31 @@
-import { createFileRoute } from "@tanstack/react-router";
-import logo from "../logo.svg";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute, useRouteContext } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-	component: App,
+  component: App,
+  loader: async ({ context }) => {
+    const queryClient = context.queryClient;
+    console.log(queryClient);
+    return queryClient.prefetchQuery({
+      queryKey: ["works"],
+      queryFn: context.eden.works.post,
+    });
+  },
 });
 
 function App() {
-	return (
-		<div className="text-center">
-			<header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-				<img
-					src={logo}
-					className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-					alt="logo"
-				/>
-				<p>
-					Edit <code>src/routes/index.tsx</code> and save to reload.
-				</p>
-				<a
-					className="text-[#61dafb] hover:underline"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<a
-					className="text-[#61dafb] hover:underline"
-					href="https://tanstack.com"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn TanStack
-				</a>
-			</header>
-		</div>
-	);
+  const { eden } = useRouteContext({
+    from: "/",
+  });
+  const { data } = useQuery({
+    queryKey: ["works"],
+    queryFn: eden.works.post,
+  });
+  return (
+    <div className="text-center">
+      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+        {data?.data?.message}
+      </header>
+    </div>
+  );
 }
