cd5c986f0b32db2eca2db048b58fd2d593c32a74
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-05-30 23:42:12 -0300

    feat(router): Add Eden context to router

--- apps/dashboard/src/router.tsx
+++ apps/dashboard/src/router.tsx
@@ -1,30 +1,33 @@
 import { createRouter as createTanstackRouter } from "@tanstack/react-router";
 import { routerWithQueryClient } from "@tanstack/react-router-with-query";
-import * as TanstackQuery from "./integrations/tanstack-query/root-provider";
-
+import * as TanstackQuery from "./integrations/tanstack-query";
+import * as Eden from "./integrations/eden";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+
 import "@packages/ui/globals.css";
+
 // Create a new router instance
 export const createRouter = () => {
-	const router = routerWithQueryClient(
-		createTanstackRouter({
-			routeTree,
-			context: {
-				...TanstackQuery.getContext(),
-			},
-			scrollRestoration: true,
-			defaultPreloadStaleTime: 0,
-		}),
-		TanstackQuery.getContext().queryClient,
-	);
+  const router = routerWithQueryClient(
+    createTanstackRouter({
+      routeTree,
+      context: {
+        ...Eden.getContext(),
+        ...TanstackQuery.getContext(),
+      },
+      scrollRestoration: true,
+      defaultPreloadStaleTime: 0,
+    }),
+    TanstackQuery.getContext().queryClient,
+  );
 
-	return router;
+  return router;
 };
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: ReturnType<typeof createRouter>;
-	}
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
 }
