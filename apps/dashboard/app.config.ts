406637b6ff246e9e504367f2fbd07c73bfd15bc4
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-05-30 23:42:03 -0300

    style(dashboard): Reformat app.config.ts

--- apps/dashboard/app.config.ts
+++ apps/dashboard/app.config.ts
@@ -1,23 +1,20 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "@tanstack/react-start/config";
-import viteTsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from '@tanstack/react-start/config'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
-	server: {
-		preset: "vercel",
-	},
-	tsr: {
-		appDirectory: "src",
-	},
-	vite: {
-		plugins: [
-			// this is the plugin that enables path aliases
-			viteTsConfigPaths({
-				projects: ["./tsconfig.json"],
-			}),
-			tailwindcss(),
-		],
-	},
-});
+  tsr: {
+    appDirectory: 'src',
+  },
+  vite: {
+    plugins: [
+      // this is the plugin that enables path aliases
+      viteTsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+      tailwindcss(),
+    ],
+  },
+})
 
-export default config;
+export default config
