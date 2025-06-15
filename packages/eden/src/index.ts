893db8c4cdb895d50fe6a152261b74ea72fb6a83
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-06-15 06:56:23 -0300

    build: Update dependencies and paths

--- packages/eden/src/index.ts
+++ packages/eden/src/index.ts
@@ -1,5 +1,5 @@
 import { treaty } from "@elysiajs/eden";
-import type { App } from "elysia-server";
+import type { App } from "@api";
 export type EdenClientType = ReturnType<typeof treaty<App>>;
 export function createEdenAdapter(url: string): EdenClientType {
   return treaty<App>(url);
