ff1a43250e6a68cd69760d078822c44fc00e9712
Manoel <manoel.carvalhoneto@tutamail.com>
2025-06-20 19:29:34 -0300

    fix(blog): Remove unused vercel adapter

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
   integrations: [mdx(), sitemap(), react()],
   site: "https://example.com",
 
