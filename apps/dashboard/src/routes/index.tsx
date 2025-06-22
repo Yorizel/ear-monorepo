89384be98157f731288a732d00d152ebb4385ed3
Manoel <manoel.carvalhoneto@tutamail.com>
2025-06-22 11:07:13 -0300

    feat(dashboard): add tamagui ui Button component

--- apps/dashboard/src/routes/index.tsx
+++ apps/dashboard/src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createQueryKey } from "@packages/eden";
+import { Button } from "@packages/tamagui-ui";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute, useRouteContext } from "@tanstack/react-router";
-
 export const Route = createFileRoute("/")({
   component: App,
 });
@@ -22,6 +22,7 @@ function App() {
       <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
         {data?.data?.message}
       </header>
+      <Button>Hello from tamagui</Button>
     </div>
   );
 }
