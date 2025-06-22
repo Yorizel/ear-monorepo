36c4d24371a1fd5d89e74eade5699061d4edf136
Manoel <manoel.carvalhoneto@tutamail.com>
2025-06-22 10:53:12 -0300

    feat(config): Add themes and update nx config

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,11 +1,11 @@
+import { themes } from "@config/theme";
 import { defaultConfig } from "@tamagui/config/v4";
 import { createTamagui } from "tamagui";
-
-export const config = createTamagui(defaultConfig);
-
+export const config = createTamagui({
+  ...defaultConfig,
+  themes,
+});
 type CustomConfig = typeof config;
-
-// ensure types work
 declare module "tamagui" {
   interface TamaguiCustomConfig extends CustomConfig {}
 }
